7c19f7747fa165ef158c022b713a2bd414aefe49
YiNing-idle <xujy6@asiainfo.com>
2020-08-31 10:33:54 +0800

    添加了echart

--- src/views/analyse/index.vue
+++ src/views/analyse/index.vue
@@ -1,8 +1,558 @@
-<template>
+in<template>
+  <div>
+    <el-row :gutter="20" class="el-row" >
+      <el-col :span="8"><div id="myChart" class="grid-content bg-color border box-shadow"  ></div></el-col>
+      <el-col :span="8"><div id="myChart1" class="grid-content bg-color border box-shadow" ></div></el-col>
+      <el-col :span="8"><div id="myChart2" class="grid-content bg-color border box-shadow" ></div></el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
+// 引入基本模板
+let echarts = require('echarts/lib/echarts')
+// 引入柱状图组件
+require('echarts/lib/chart/bar')
+// 引入提示框和title组件
+require('echarts/lib/component/tooltip')
+require('echarts/lib/component/title')
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted() {
+    this.drawLine();
+  },
+  methods: {
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = echarts.init(document.getElementById('myChart'))
+      // 绘制图表
+      var posList = [
+        'left', 'right', 'top', 'bottom',
+        'inside',
+        'insideTop', 'insideLeft', 'insideRight', 'insideBottom',
+        'insideTopLeft', 'insideTopRight', 'insideBottomLeft', 'insideBottomRight'
+      ];
+
+      app.configParameters = {
+        rotate: {
+          min: -90,
+          max: 90
+        },
+        align: {
+          options: {
+            left: 'left',
+            center: 'center',
+            right: 'right'
+          }
+        },
+        verticalAlign: {
+          options: {
+            top: 'top',
+            middle: 'middle',
+            bottom: 'bottom'
+          }
+        },
+        position: {
+          options: echarts.util.reduce(posList, function (map, pos) {
+            map[pos] = pos;
+            return map;
+          }, {})
+        },
+        distance: {
+          min: 0,
+          max: 100
+        }
+      };
+
+      app.config = {
+        rotate: 90,
+        align: 'left',
+        verticalAlign: 'middle',
+        position: 'insideBottom',
+        distance: 15,
+        onChange: function () {
+          var labelOption = {
+            normal: {
+              rotate: app.config.rotate,
+              align: app.config.align,
+              verticalAlign: app.config.verticalAlign,
+              position: app.config.position,
+              distance: app.config.distance
+            }
+          };
+          myChart.setOption({
+            series: [{
+              label: labelOption
+            }, {
+              label: labelOption
+            }, {
+              label: labelOption
+            }, {
+              label: labelOption
+            }]
+          });
+        }
+      };
+
+
+      var labelOption = {
+        show: true,
+        position: app.config.position,
+        distance: app.config.distance,
+        align: app.config.align,
+        verticalAlign: app.config.verticalAlign,
+        rotate: app.config.rotate,
+        formatter: '{c}  {name|{a}}',
+        fontSize: 16,
+        rich: {
+          name: {
+            textBorderColor: '#fff'
+          }
+        }
+      };
+
+      myChart.setOption({
+
+        color: ['#003366', '#006699', '#4cabce', '#e5323e'],
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        legend: {
+          data: ['Forest', 'Steppe', 'Desert', 'Wetland']
+        },
+        toolbox: {
+          show: true,
+          orient: 'vertical',
+          left: 'right',
+          top: 'center',
+          feature: {
+            mark: {show: true},
+            dataView: {show: true, readOnly: false},
+            magicType: {show: true, type: ['line', 'bar', 'stack', 'tiled']},
+            restore: {show: true},
+            saveAsImage: {show: true}
+          }
+        },
+        xAxis: [
+          {
+            type: 'category',
+            axisTick: {show: false},
+            data: ['2012', '2013', '2014', '2015', '2016']
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: 'Forest',
+            type: 'bar',
+            barGap: 0,
+            label: labelOption,
+            data: [320, 332, 301, 334, 390]
+          },
+          {
+            name: 'Steppe',
+            type: 'bar',
+            label: labelOption,
+            data: [220, 182, 191, 234, 290]
+          },
+          {
+            name: 'Desert',
+            type: 'bar',
+            label: labelOption,
+            data: [150, 232, 201, 154, 190]
+          },
+          {
+            name: 'Wetland',
+            type: 'bar',
+            label: labelOption,
+            data: [98, 77, 101, 99, 40]
+          }
+        ]
+      });
+      window.addEventListener("resize", function () {
+        myChart.resize();
+      });
+
+      let myChart1 = echarts.init(document.getElementById('myChart1'))
+      myChart1.setOption({
+        tooltip: {
+          formatter: "{a} <br/>{c} {b}"
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            restore: {show: true},
+            saveAsImage: {show: true}
+          }
+        },
+        series : [
+          {
+            name: '速度',
+            type: 'gauge',
+            z: 3,
+            min: 0,
+            max: 220,
+            splitNumber: 11,
+            radius: '50%',
+            axisLine: {            // 坐标轴线
+              lineStyle: {       // 属性lineStyle控制线条样式
+                width: 10
+              }
+            },
+            axisTick: {            // 坐标轴小标记
+              length: 15,        // 属性length控制线长
+              lineStyle: {       // 属性lineStyle控制线条样式
+                color: 'auto'
+              }
+            },
+            splitLine: {           // 分隔线
+              length: 20,         // 属性length控制线长
+              lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
+                color: 'auto'
+              }
+            },
+            axisLabel: {
+              backgroundColor: 'auto',
+              borderRadius: 2,
+              color: '#eee',
+              padding: 3,
+              textShadowBlur: 2,
+              textShadowOffsetX: 1,
+              textShadowOffsetY: 1,
+              textShadowColor: '#222'
+            },
+            title: {
+              // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+              fontWeight: 'bolder',
+              fontSize: 20,
+              fontStyle: 'italic'
+            },
+            detail: {
+              // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+              formatter: function (value) {
+                value = (value + '').split('.');
+                value.length < 2 && (value.push('00'));
+                return ('00' + value[0]).slice(-2)
+                    + '.' + (value[1] + '00').slice(0, 2);
+              },
+              fontWeight: 'bolder',
+              borderRadius: 3,
+              backgroundColor: '#444',
+              borderColor: '#aaa',
+              shadowBlur: 5,
+              shadowColor: '#333',
+              shadowOffsetX: 0,
+              shadowOffsetY: 3,
+              borderWidth: 2,
+              textBorderColor: '#000',
+              textBorderWidth: 2,
+              textShadowBlur: 2,
+              textShadowColor: '#fff',
+              textShadowOffsetX: 0,
+              textShadowOffsetY: 0,
+              fontFamily: 'Arial',
+              width: 100,
+              color: '#eee',
+              rich: {}
+            },
+            data: [{value: 40, name: 'km/h'}]
+          },
+          {
+            name: '转速',
+            type: 'gauge',
+            center: ['20%', '55%'],    // 默认全局居中
+            radius: '35%',
+            min: 0,
+            max: 7,
+            endAngle: 45,
+            splitNumber: 7,
+            axisLine: {            // 坐标轴线
+              lineStyle: {       // 属性lineStyle控制线条样式
+                width: 8
+              }
+            },
+            axisTick: {            // 坐标轴小标记
+              length: 12,        // 属性length控制线长
+              lineStyle: {       // 属性lineStyle控制线条样式
+                color: 'auto'
+              }
+            },
+            splitLine: {           // 分隔线
+              length: 20,         // 属性length控制线长
+              lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
+                color: 'auto'
+              }
+            },
+            pointer: {
+              width: 5
+            },
+            title: {
+              offsetCenter: [0, '-30%'],       // x, y，单位px
+            },
+            detail: {
+              // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+              fontWeight: 'bolder'
+            },
+            data: [{value: 1.5, name: 'x1000 r/min'}]
+          },
+          {
+            name: '油表',
+            type: 'gauge',
+            center: ['77%', '50%'],    // 默认全局居中
+            radius: '25%',
+            min: 0,
+            max: 2,
+            startAngle: 135,
+            endAngle: 45,
+            splitNumber: 2,
+            axisLine: {            // 坐标轴线
+              lineStyle: {       // 属性lineStyle控制线条样式
+                width: 8
+              }
+            },
+            axisTick: {            // 坐标轴小标记
+              splitNumber: 5,
+              length: 10,        // 属性length控制线长
+              lineStyle: {        // 属性lineStyle控制线条样式
+                color: 'auto'
+              }
+            },
+            axisLabel: {
+              formatter: function (v){
+                switch (v + '') {
+                  case '0' : return 'E';
+                  case '1' : return 'Gas';
+                  case '2' : return 'F';
+                }
+              }
+            },
+            splitLine: {           // 分隔线
+              length: 15,         // 属性length控制线长
+              lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
+                color: 'auto'
+              }
+            },
+            pointer: {
+              width: 2
+            },
+            title: {
+              show: false
+            },
+            detail: {
+              show: false
+            },
+            data: [{value: 0.5, name: 'gas'}]
+          },
+          {
+            name: '水表',
+            type: 'gauge',
+            center: ['77%', '50%'],    // 默认全局居中
+            radius: '25%',
+            min: 0,
+            max: 2,
+            startAngle: 315,
+            endAngle: 225,
+            splitNumber: 2,
+            axisLine: {            // 坐标轴线
+              lineStyle: {       // 属性lineStyle控制线条样式
+                width: 8
+              }
+            },
+            axisTick: {            // 坐标轴小标记
+              show: false
+            },
+            axisLabel: {
+              formatter: function(v){
+                switch (v + '') {
+                  case '0' : return 'H';
+                  case '1' : return 'Water';
+                  case '2' : return 'C';
+                }
+              }
+            },
+            splitLine: {           // 分隔线
+              length: 15,         // 属性length控制线长
+              lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
+                color: 'auto'
+              }
+            },
+            pointer: {
+              width:2
+            },
+            title: {
+              show: false
+            },
+            detail: {
+              show: false
+            },
+            data: [{value: 0.5, name: 'gas'}]
+          }
+        ]
+      });
+
+      setInterval(function (){
+        option.series[0].data[0].value = (Math.random()*100).toFixed(2) - 0;
+        option.series[1].data[0].value = (Math.random()*7).toFixed(2) - 0;
+        option.series[2].data[0].value = (Math.random()*2).toFixed(2) - 0;
+        option.series[3].data[0].value = (Math.random()*2).toFixed(2) - 0;
+        myChart1.setOption(option,true);
+      },2000);
+      window.addEventListener("resize", function () {
+        myChart1.resize();
+      });
+
+
+
+
+      let myChart2 = echarts.init(document.getElementById('myChart2'))
+      myChart2.setOption({
+
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b}: {c} ({d}%)'
+        },
+        legend: {
+          orient: 'vertical',
+          left: 10,
+          data: ['直达', '营销广告', '搜索引擎', '邮件营销', '联盟广告', '视频广告', '百度', '谷歌', '必应', '其他']
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            selectedMode: 'single',
+            radius: [0, '30%'],
+
+            label: {
+              position: 'inner'
+            },
+            labelLine: {
+              show: false
+            },
+            data: [
+              {value: 335, name: '直达', selected: true},
+              {value: 679, name: '营销广告'},
+              {value: 1548, name: '搜索引擎'}
+            ]
+          },
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['40%', '55%'],
+            label: {
+              formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
+              backgroundColor: '#eee',
+              borderColor: '#aaa',
+              borderWidth: 1,
+              borderRadius: 4,
+              // shadowBlur:3,
+              // shadowOffsetX: 2,
+              // shadowOffsetY: 2,
+              // shadowColor: '#999',
+              // padding: [0, 7],
+              rich: {
+                a: {
+                  color: '#999',
+                  lineHeight: 22,
+                  align: 'center'
+                },
+                // abg: {
+                //     backgroundColor: '#333',
+                //     width: '100%',
+                //     align: 'right',
+                //     height: 22,
+                //     borderRadius: [4, 4, 0, 0]
+                // },
+                hr: {
+                  borderColor: '#aaa',
+                  width: '100%',
+                  borderWidth: 0.5,
+                  height: 0
+                },
+                b: {
+                  fontSize: 16,
+                  lineHeight: 33
+                },
+                per: {
+                  color: '#eee',
+                  backgroundColor: '#334455',
+                  padding: [2, 4],
+                  borderRadius: 2
+                }
+              }
+            },
+            data: [
+              {value: 335, name: '直达'},
+              {value: 310, name: '邮件营销'},
+              {value: 234, name: '联盟广告'},
+              {value: 135, name: '视频广告'},
+              {value: 1048, name: '百度'},
+              {value: 251, name: '谷歌'},
+              {value: 147, name: '必应'},
+              {value: 102, name: '其他'}
+            ]
+          }
+        ]
+      });
+      window.addEventListener("resize", function () {
+        myChart2.resize();
+      });
+
+    }
+  }
+}
 </script>
 
 <style>
+.el-row {
+  height: 100px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-wrap: wrap
+}
+.box-shadow{
+  box-shadow: 0 2px 4px rgba(0,0,0,.12),0 0 6px rgba(0,0,0,.04);
+}
+
+.bg-color {
+  background: #ffffff;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.border{
+  border-radius: 10px;
+}
+
+#myChart{
+  height:500px;
+  margin-top:50px;
+  margin-left: 30px;
+
+}
+
+#myChart1{
+  height:500px;
+  margin-top:50px;
+}
+#myChart2{
+  height:500px;
+  margin-top:50px;
+
+  margin-right: 30px;
+}
+
 </style>
